c6cf295233ed5c45b76c5cedc4e631508d28b931
KListrian <leirf1000-2@yahoo.se>
2026-06-06 20:38:18 +0200

    changed send_str in 6502 to subroutine

--- sram.asm
+++ sram.asm
@@ -1,28 +1,47 @@
+ptr = $20
+
 *=$0000
 start	ldx #$77
 		jmp	start
 
 *=$01c0
-send	ldy #$00
-next	lda resettext,y
-		beq inf         ; End of string? Go wait for 'a'
-		sta byte_out    ; Place byte for Pi
-		lda #$01
-		sta status      ; Signal Pi to send
-		jsr waitz       ; Wait for Pi to finish
-		iny             ; Next character
-		jmp next
+reset_entry:
+        lda #<resettext
+        sta ptr
+        lda #>resettext
+        sta ptr+1
+        jsr send_str
 
 inf		lda byte_in
-		cmp #97
+		cmp #13
 		bne inf
 		lda #0
 		sta byte_in
-		jmp send
+        
+        lda #<helloworld
+        sta ptr
+        lda #>helloworld
+        sta ptr+1
+        jsr send_str
+        jmp inf
 
-waitz	lda status
-		bne waitz
-		rts
+; Subroutine: send_str
+; Argument: Pointer to null-terminated string in 'ptr' ($20/$21)
+send_str:
+        ldy #$00
+next_ch:
+        lda (ptr),y     ; Read char from string pointer
+        beq done        ; End of string (0)?
+        sta byte_out    ; Place byte for Pi
+        lda #$01
+        sta status      ; Signal Pi to send
+wait:   lda status      ; Wait for Pi to finish
+        bne wait
+        iny
+        bne next_ch
+        inc ptr+1       ; Handle page wrap for long strings
+        jmp next_ch
+done:   rts
 
 *=$1c00
 byte_out .byte $0
@@ -31,6 +50,8 @@ byte_in  .byte $0
 
 resettext 	.text 27,"[36m","*** 6502 Retro processor ***",27,"[0m",13,10
 		  	.text "Ready",13,10,0
+
+helloworld	.text 27,"[36m","Hello World!",27,"[0m",13,10,0
 
 *=$7ffa
 		.byte $c0,$01	; nmi
