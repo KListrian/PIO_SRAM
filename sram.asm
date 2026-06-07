6333460a682abd5f4cc54b7965e9e8f96628d113
KListrian <leirf1000-2@yahoo.se>
2026-06-07 17:14:14 +0200

    added comments

--- sram.asm
+++ sram.asm
@@ -5,10 +5,12 @@ byte_in  = $1C02
 
 ; --- Zero Page ---
 ptr      = $20
+range_ptr = $22
+limit_ptr = $24
 
 *=$0000
 start	ldx #$77
-		jmp	start
+	jmp	start
 
 *=$01c0
 reset_entry:
@@ -24,11 +26,11 @@ reset_entry:
         sta ptr+1
         jsr send_str
 
-inf		lda byte_in
-		cmp #97
-		bne inf
-		lda #0
-		sta byte_in
+inf	lda byte_in     ; check for byte in
+	cmp #97         ; 'a' key to trigger send hello world
+	bne inf
+	lda #0
+	sta byte_in     ; clear byte_in
         
         lda #<helloworld
         sta ptr
@@ -45,15 +47,61 @@ next_ch:
         lda (ptr),y     ; Read char from string pointer
         beq done        ; End of string (0)?
         sta byte_out    ; Place byte for Pi
-        lda #$01
-        sta status      ; Signal Pi to send
-wait:   lda status      ; Wait for Pi to finish
-        bne wait
+        jsr send_char
         iny
         bne next_ch
         inc ptr+1       ; Handle page wrap for long strings
         jmp next_ch
 done:   rts
+
+; Subroutine: dump_range
+; Sends memory range [range_ptr] to [limit_ptr] (inclusive) as hex text
+dump_range:
+        ldy #$00
+dump_loop:
+        lda (range_ptr),y
+        jsr print_hex_byte
+        
+        lda #$20        ; Space separator
+        jsr send_char
+
+        ; Check if range_ptr == limit_ptr
+        lda range_ptr
+        cmp limit_ptr
+        bne inc_range
+        lda range_ptr+1
+        cmp limit_ptr+1
+        beq dump_done   ; Reached the end
+
+inc_range:
+        inc range_ptr
+        bne dump_loop
+        inc range_ptr+1
+        jmp dump_loop
+dump_done:
+        rts
+
+print_hex_byte:
+        pha             ; Save original byte
+        lsr             ; Shift high nibble to low
+        lsr
+        lsr
+        lsr
+        jsr conv_send   ; Convert and send high nibble
+        pla             ; Restore original byte
+        and #$0F        ; Mask for low nibble
+conv_send:
+        cmp #$0A
+        bcc is_num
+        adc #$06        ; Convert to A-F (Carry is set)
+is_num: adc #$30        ; Convert to '0'-'9'
+send_char:
+        sta byte_out
+        lda #$01
+        sta status      ; Signal Pi to send
+tx_wait:lda status      ; Wait for Pi to finish
+        bne tx_wait
+        rts
 
 *=$1c00
         .byte $00       ; reserved for byte_out
